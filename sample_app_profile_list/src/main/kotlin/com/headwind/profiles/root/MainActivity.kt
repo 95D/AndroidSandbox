d2217826fc417295759e40308364dcedee67a6f8
95D <avos170@gmail.com>
2024-03-23 20:40:57 +0900

    Create MainActivity

---
+++ sample_app_profile_list/src/main/kotlin/com/headwind/profiles/root/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.headwind.profiles.root
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * A main activity of sample app
+ */
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Profile list sample app")
+        }
+    }
+}
