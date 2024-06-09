1bc2b62b7b59a63fd1a3a00a671705969858726f
95D <avos170@gmail.com>
2024-06-09 16:10:34 +0900

    Create method getGearShape

---
+++ gear-ui/src/main/java/com/example/gear/ui/shape/GetGearShape.kt
@@ -0,0 +1,84 @@
+package com.example.gear.ui.shape
+
+import android.graphics.PointF
+import androidx.core.graphics.times
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.RoundedPolygon
+import kotlin.math.cos
+import kotlin.math.sin
+
+fun getGearShape(
+    numTeeth: Int,
+    teethRadius: Float,
+    wheelRadius: Float,
+    centerX: Float,
+    centerY: Float,
+    toothRounding: CornerRounding = CornerRounding.Unrounded
+): RoundedPolygon = RoundedPolygon(
+    vertices = getGearVertices(
+        numVertices = numTeeth,
+        wheelRadius = wheelRadius,
+        teethRadius = teethRadius,
+        centerX = centerX,
+        centerY = centerY
+    ),
+    perVertexRounding = cornerRoundings(
+        toothRounding = toothRounding,
+        numVertices = numTeeth
+    )
+)
+
+private fun cornerRoundings(
+    toothRounding: CornerRounding,
+    numVertices: Int
+): List<CornerRounding> =
+    listOf(
+        CornerRounding.Unrounded,
+        toothRounding,
+        toothRounding,
+        CornerRounding.Unrounded,
+    ).take(numVertices)
+
+private fun getGearVertices(
+    numVertices: Int,
+    teethRadius: Float,
+    wheelRadius: Float,
+    centerX: Float,
+    centerY: Float
+): FloatArray {
+    val result = FloatArray(numVertices * 8)
+    var arrayIndex = 0
+    for (i in 0 until numVertices) {
+        val radiusList = listOf(
+            wheelRadius,
+            teethRadius,
+            teethRadius,
+            wheelRadius
+        )
+        for (j in 0 until 4) {
+            val pointIndex = (i * 4 + j).toFloat()
+            val vertex = radialToCartesian(
+                radiusList[j],
+                (Math.PI.toFloat() * 2) / (numVertices * 4) * pointIndex
+            ) + PointF(centerX, centerY)
+            result[arrayIndex++] = vertex.x
+            result[arrayIndex++] = vertex.y
+        }
+    }
+    return result
+}
+
+internal fun radialToCartesian(
+    radius: Float,
+    angleRadians: Float,
+    center: PointF = PointF(0f, 0f)
+): PointF = directionVector(angleRadians) * radius + center
+
+internal fun directionVector(angleRadians: Float): PointF =
+    PointF(cos(angleRadians), sin(angleRadians))
+
+internal operator fun PointF.plus(another: PointF): PointF =
+    PointF(x + another.x, y + another.y)
+
+internal operator fun PointF.times(another: Float): PointF =
+    PointF(x * another, y * another)
