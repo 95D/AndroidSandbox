2023942e2a001ff4afad8a57e3772e0184d9cef1
95D <avos170@gmail.com>
2025-02-22 22:07:20 +0900

    Update gear-ui build script - To be library module

--- gear-ui/build.gradle.kts
+++ gear-ui/build.gradle.kts
@@ -1,52 +1,15 @@
 import com.headwind.androidsandbox.AppBuildType
 
 plugins {
-    id("androidsandbox.android.application")
-    id("androidsandbox.android.application.compose")
-    id("androidsandbox.android.application.flavors")
-    id("androidsandbox.android.application.jacoco")
+    id("androidsandbox.android.library")
+    id("androidsandbox.android.library.compose")
+    id("androidsandbox.android.library.jacoco")
     id("androidsandbox.android.hilt")
 }
 
 android {
-    defaultConfig {
-        applicationId = "com.example.androidsandbox"
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        debug {
-            applicationIdSuffix = AppBuildType.DEBUG.applicationIdSuffix
-        }
-        val release by getting {
-            isMinifyEnabled = true
-            applicationIdSuffix = AppBuildType.RELEASE.applicationIdSuffix
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-
-            // To publish on the Play store a private signing key is required, but to allow anyone
-            // who clones the code to sign and run the release variant, use the debug signing key.
-            // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-        create("benchmark") {
-            // Enable all the optimizations from release build through initWith(release).
-            initWith(release)
-            matchingFallbacks.add("release")
-            // Debug key signing is available to everyone.
-            signingConfig = signingConfigs.getByName("debug")
-            // Only use benchmark proguard rules
-            proguardFiles("benchmark-rules.pro")
-            isMinifyEnabled = true
-            applicationIdSuffix = AppBuildType.BENCHMARK.applicationIdSuffix
-        }
-    }
     namespace = "com.example.androidsandbox"
+    resourcePrefix = "gear_ui_"
 }
 
 dependencies {
